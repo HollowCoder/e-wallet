3505f963b5a4c709768d0de767b5dad36740f74d
Unknown <andrew.oleary13@stjohns.edu>
2019-05-01 01:49:42 -0400

    New

---
+++ app/forms.py
@@ -0,0 +1,71 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, BooleanField, SubmitField, DateField, IntegerField
+from wtforms.validators import DataRequired, ValidationError, Email, EqualTo
+from app.models import User
+from dateutil import *
+from datetime import *
+
+class RegistrationForm(FlaskForm):
+	# username = StringField('Email', validators=[DataRequired()])
+	email = StringField('Email', validators=[DataRequired(), Email()])
+	password = PasswordField('Password', validators=[DataRequired()])
+	retypePassword = PasswordField('Retype Password', validators=[DataRequired(), EqualTo('password')])
+	submit = SubmitField('Register')
+
+	def validate_email(self, email):
+		user = User.query.filter_by(email=email.data).first()
+		if user is not None:
+			raise ValidationError('Please use a different email address')
+
+
+class LoginForm(FlaskForm):
+	email = StringField('Email', validators=[DataRequired()])
+	password = PasswordField('Password', validators=[DataRequired()])
+	remember_me = BooleanField('Remember Me')
+	submit = SubmitField('Sign In')
+
+class AddCustomerForm(FlaskForm):
+	email = StringField('Email', validators=[DataRequired()])
+	number = StringField('Number', validators=[DataRequired()])
+	addressState = StringField('State', validators=[DataRequired()])
+	expMonth = StringField('Expiration Month', validators=[DataRequired()])
+	expYear = StringField('Expiration Year', validators=[DataRequired()])
+	addressCity = StringField('City', validators=[DataRequired()])
+	addressZip = StringField('Zip Code', validators=[DataRequired()])
+	cvv = StringField('CVV', validators=[DataRequired()])
+	submit = SubmitField('Add Card')
+
+class AddCardForm(FlaskForm):
+	number = StringField('Number', validators=[DataRequired()])
+	addressState = StringField('State', validators=[DataRequired()])
+	expMonth = StringField('Expiration Month', validators=[DataRequired()])
+	expYear = StringField('Expiration Year', validators=[DataRequired()])
+	addressCity = StringField('City', validators=[DataRequired()])
+	addressZip = StringField('Zip Code', validators=[DataRequired()])
+	cvv = StringField('CVV', validators=[DataRequired()])
+	submit = SubmitField('Add Card')
+
+class PayBillForm(FlaskForm):
+	amount = StringField("Amount", validators=[DataRequired()])
+	description = StringField("Description", validators=[DataRequired()])
+	submit = SubmitField('Pay')
+
+# def validate_username(self, username):
+# 	user = User.query.filter_by(username=username.data).first()
+# 	if user is not None:
+# 		raise ValidationError('Please use a different username')
+
+class UserSettings(FlaskForm):
+	emailNotifications = BooleanField('Email Notifications')
+	smsNotifications = BooleanField('SMS Notifications')
+	submit = SubmitField('Submit')
+
+class ScheduledPayments(FlaskForm):
+	bill = PayBillForm()
+	dueDate = DateField(label='Due Date:')
+	#RelativeDate('Due Date')
+	submit = SubmitField('Submit')
+
+class RelativeDate(Field):
+	today = DateField(label='Todays Date:', format=datetime.now)
+	#def __init__(self, label='', validators=None, **kwargs)
